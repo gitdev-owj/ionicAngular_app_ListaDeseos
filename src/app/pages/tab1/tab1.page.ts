8aeae18d2f9ff24c918649cd53e6e9737e2eba0c
Oscar W Jimenez <oscawj@gmail.com>
2020-09-18 22:49:17 -0500

    se adiciona opción

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { DeseosService } from '../../services/deseos.service';
 
 @Component({
@@ -8,8 +9,14 @@ import { DeseosService } from '../../services/deseos.service';
 })
 export class Tab1Page {
 
-  constructor( public deseoService: DeseosService ) {
+  constructor( public deseoService: DeseosService,
+               private router: Router) {
     
+  }
+
+  agregarLista(){
+
+    this.router.navigateByUrl('/tabs/tab1/agregar');
   }
 
 }
