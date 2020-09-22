0ba473f118c40c225b614b4686e8c1892ecae8e2
Oscar W Jimenez <oscawj@gmail.com>
2020-09-21 22:35:18 -0500

    se adicionan cambios para las tareas

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { DeseosService } from '../../services/deseos.service';
 
 @Component({
@@ -10,13 +11,46 @@ import { DeseosService } from '../../services/deseos.service';
 export class Tab1Page {
 
   constructor( public deseoService: DeseosService,
-               private router: Router) {
+               private router: Router,
+               private alertCtrl: AlertController) {
     
   }
 
-  agregarLista(){
+  async agregarLista(){
 
-    this.router.navigateByUrl('/tabs/tab1/agregar');
+    //this.router.navigateByUrl('/tabs/tab1/agregar');
+    const alert = await this.alertCtrl.create({
+      header: 'Nueva Lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Nombre de la lista'
+        }
+       ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'Cancel',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        },
+        {
+          text: 'Crear',
+          handler: (data) => {
+            console.log(data);
+            if(data.titulo.length === 0 ){
+              return;
+            }
+             
+            this.deseoService.crearLista(data.titulo);
+            //crear la lista 
+
+          }
+        }
+      ]
+    });
+    alert.present();
   }
-
 }
